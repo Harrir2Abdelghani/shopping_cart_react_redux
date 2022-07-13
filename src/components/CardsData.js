77215fa8059af69e89cc92e3f9cf4e6ffdd79d82
Abdelghani <h.harrir@esi-sba.dz>
2022-07-13 15:47:14 +0200

    shopping_cart

---
+++ src/components/CardsData.js
@@ -0,0 +1,22 @@
+const Cardsdata = [{
+        id: 1,
+        rname: "FRENCH BREAD",
+        imgdata: "https://tmbidigitalassetsazure.blob.core.windows.net/toh/GoogleImagesPostCard/Sourdough-French-Bread_EXPS_FTTS20_15241_B03_06_1b.jpg",
+        price: 1,
+    },
+    {
+        id: 2,
+        rname: "Fresh Swiss Milk",
+        imgdata: "https://classicdeli.vn/ho-chi-minh/528-full_default/premium-swiss-milk-emmi-15-fat-semi-skimmed-1l.jpg",
+        price: 1.15,
+    },
+    {
+        id: 3,
+        rname: "Butter",
+        imgdata: "https://dictionary.cambridge.org/fr/images/thumb/butter_noun_002_05056.jpg?version=5.0.247",
+        price: 0.8,
+    },
+
+];
+
+export default Cardsdata;
